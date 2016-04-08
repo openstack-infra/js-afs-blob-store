b900ee45ff9305417aca0ab870fb92bb9080b6c0
Michael Krotscheck <krotscheck@gmail.com>
2016-04-08 10:30:55 -0700

    Added unit tests for the blobstore rewrites
    100% test coverage.

--- index.js
+++ index.js
@@ -4,14 +4,39 @@
   var options = require('./lib/args');
   var blobstore = require('fs-blob-store')(options.dir);
 
-  function afsify (opts) {
+  function clone (instance) {
+    return JSON.parse(JSON.stringify(instance));
+  }
+
+  function ensort (opts) {
+    // First clone the instance.
+    opts = clone(opts);
+
     if (typeof opts === 'string') {
       // Packages that start with alphanumerics are rewritten:
       // foo -> f/foo, bar -> b/bar
-      opts = opts.replace(/^\/?([a-zA-Z0-9])/, function(match, submatch) {
-        return submatch + '/' + submatch;
+      return opts.replace(/^\/?([a-zA-Z0-9])/, function(match, submatch) {
+        return match + '/' + submatch;
       });
-      return opts;
+    } else if (opts.hasOwnProperty('key')) {
+      opts.key = ensort(opts.key); // Yes this is recursion, what of it?
+    }
+
+    return opts;
+  }
+
+  function desort (opts) {
+    // First clone the instance.
+    opts = clone(opts);
+
+    if (typeof opts === 'string') {
+      // Packages that start with alphanumerics are rewritten:
+      // foo -> f/foo, bar -> b/bar
+      return opts.replace(/^(\/?([a-zA-Z0-9]))\/\2/, function(match, s1) {
+        return s1;
+      });
+    } else if (opts.hasOwnProperty('key')) {
+      opts.key = desort(opts.key);
     }
 
     return opts;
@@ -19,16 +44,20 @@
 
   module.exports = {
     createWriteStream: function(opts, cb) {
-      return blobstore.createWriteStream(afsify(opts), cb);
+      return blobstore.createWriteStream(ensort(opts), function(results) {
+        if (cb) {
+          return cb(desort(results));
+        }
+      });
     },
     createReadStream: function(key, opts) {
-      return blobstore.createReadStream(afsify(key), opts);
+      return blobstore.createReadStream(ensort(key), opts);
     },
     exists: function(opts, cb) {
-      return blobstore.exists(afsify(opts), cb);
+      return blobstore.exists(ensort(opts), cb);
     },
     remove: function(opts, cb) {
-      return blobstore.remove(afsify(opts), cb);
+      return blobstore.remove(ensort(opts), cb);
     }
   };
 })();
